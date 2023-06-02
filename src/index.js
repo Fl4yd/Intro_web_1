86aa2f7c32d72aca66a02bdce925f9324fb34adb
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-02 05:08:26 +0000

    Tehtava 4.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import { tokenToString } from "typescript";
 import "./styles.css";
 
 const myButton = document.getElementById('my-button');
 const myButton2 = document.getElementById('add-data'); 
 const hello = document.getElementById('hello');
 const lista = document.getElementById('lista');
-//const teksti = document.getElementById('teksti');
+const teksti = document.getElementById('teksti');
 
 
 myButton.onclick = function() {
@@ -12,7 +13,7 @@ myButton.onclick = function() {
   hello.innerHTML='My notebook'
 };
 myButton2.onclick = function() {
-  lista.innerHTML = "jotain"
+  lista.innerHTML = "<li>jotain</li><li>" + teksti.value + "</li>"
   //lista.innerHTML = "<li>" + teksti.value + "</li>";
 }
 
