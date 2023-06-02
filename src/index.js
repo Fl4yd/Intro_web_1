2117008afda900aa2ddc329c0e560ef72cdf47e6
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-02 04:38:19 +0000

    Assignment 1 ready code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const myButton = document.getElementById('my-button');
+const myButton2 = document.getElementById('add-data'); 
+const hello = document.getElementById('hello');
+const lista = document.getElementById('lista');
+const teksti = document.getElementById('teksti');
+
+
+myButton.onclick = function() {
+  console.log('Hello world')
+  hello.innerHTML='My notebook'
+};
+myButton2.onclick = function() {
+  lista.innerHTML = "<li>" + teksti.value + "</li>";
+}
+
